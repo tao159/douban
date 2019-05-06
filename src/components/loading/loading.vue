01955bb2d148872bec3594f56b5e4b778e1a401d
tao159 <yangtao155@163.com>
2019-05-06 19:05:23 +0800

    recommend页面开发

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="loading">
+        <img src="../../common/images/loading_green.gif" alt="">
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+.loading
+    width:100%;
+    height:60px;
+    background:transparent;
+    text-align:center;
+    img 
+        width:40px
+</style>
--- src/views/recommend/recommend.vue
+++ src/views/recommend/recommend.vue
@@ -1,15 +1,75 @@
 <template>
-    <div>
-        
+    <div class="recommend">
+        <loading v-show="!recommendList.length"></loading>
+        <div class="recommend-content">
+            <a href="" v-for="(item,index) in recommendList" :key="index">
+                <div class="imgWrapper">
+                    <div class="larger-img">
+                        <img  v-if="item.img.length" :src='"https://images.weserv.nl/?url="+item.img[0].image.normal.url' alt="">
+                    </div>
+                    <div class="left-img">
+                        <img  v-if="item.img.length" :src='"https://images.weserv.nl/?url="+item.img[1].image.normal.url' alt="">
+                        <img  v-if="item.img.length" :src='"https://images.weserv.nl/?url="+item.img[2].image.normal.url' alt="">
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 </template>
 
 <script>
+import {getRecommend} from 'api/recommend'
+import Loading from '@/components/loading/loading'
 export default {
-    
+    data(){
+        return{
+            recommendList:[]
+        }
+    },
+    methods:{
+        _recommend(){
+            
+            getRecommend().then(res=>{
+                
+                const _this=this
+                res.items.forEach((item)=>{ 
+                   
+                   _this.recommendList.push({
+                       title:item.content.title,
+                       abstract:item.content.abstract,
+                       url:item.content.url,
+                       img:item.content.photos.length!=0?item.content.photos:[]
+                   })
+                })
+              
+                console.log(this.recommendList)
+            })
+        }
+    },
+    components:{
+        Loading
+    },
+    mounted(){
+        const _this=this
+       setTimeout(()=>{
+           _this._recommend()
+       },200)
+       
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
+.recommend
+    margin-top:47px
+    a
+        display:block
+        padding:25px 18px 25px 0
+        .imgWrapper
+            display:flex
+            .larger-img
+                flex:3.2
+            .left-img
+                flex:1
 
 </style>
